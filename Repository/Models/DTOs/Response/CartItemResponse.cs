f47b6a63543c063c6f4baff466ccba3ffbab6e1a
Van Quoc Thinh <thinhvan.231003@gmail.com>
2025-05-24 20:58:11 +0700

    cors

---
+++ Repository/Models/DTOs/Response/CartItemResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository.Models.DTOs.Response
+{
+    public class CartItemResponse
+    {
+        public Guid Id { get; set; }
+        public int? Quantity { get; set; }
+        public double? UnitPrice { get; set; }
+        public CustomizeResponse? Customize { get; set; }
+    }
+}
