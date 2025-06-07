579298236a7058822fa68a4af850f6c2de9896dc
Van Quoc Thinh <thinhvan.231003@gmail.com>
2025-06-07 15:40:47 +0700

    update cart

--- Repository/Models/DTOs/Response/CartItemResponse.cs
+++ Repository/Models/DTOs/Response/CartItemResponse.cs
@@ -6,7 +6,7 @@
         public int? Quantity { get; set; }
         public double? UnitPrice { get; set; }
         public string? Description { get; set; }
-        public CustomizeResponse? Customize { get; set; }
+     //   public CustomizeResponse? Customize { get; set; }
 
     }
 }
