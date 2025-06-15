b82c5b2eccc2aef8a65edfdded0d982c96ed8901
Van Quoc Thinh <thinhvan.231003@gmail.com>
2025-06-15 15:53:10 +0700

    update payOs

---
+++ Cafe_Web_App/Migrations/20250614105331_updateOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cafe_Web_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Orders");
+        }
+    }
+}
